a8d49292296e70cd77249600de66c57bf90c9d98
Youssef-At <72509411+Youssef-At@users.noreply.github.com>
2023-08-17 03:11:15 +0100

    Update 103-python.c

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -2,26 +2,69 @@
 #include <Python.h>
 
 /**
- * print_python_list_info - prints python list info
+ * print_python_bytes - Prints bytes information
  *
- * @p: PyObject
+ * @p: Python Object
  * Return: no return
  */
-void print_python_list_info(PyObject *p)
+void print_python_bytes(PyObject *p)
 {
-	long int sz, i;
-	PyListObject *ls;
-	PyObject *tm;
+	char *string;
+	long int size, i, limit;
 
-	sz = Py_SIZE(p);
-	printf("[*] Size of the Python List = %ld\n", sz);
-
-	ls = (PyListObject *)p;
-	printf("[*] Allocated = %ld\n", ls->allocated);
-
-	for (i = 0; i < sz; i++)
+	printf("[.] bytes object info\n");
+	if (!PyBytes_Check(p))
 	{
-		tm = PyList_GetItem(p, i);
-		printf("Element %ld: %s\n", i, Py_TYPE(tm)->tp_name);
+		printf("  [ERROR] Invalid Bytes Object\n");
+		return;
+	}
+
+	size = ((PyVarObject *)(p))->ob_size;
+	string = ((PyBytesObject *)p)->ob_sval;
+
+	printf("  size: %ld\n", size);
+	printf("  trying string: %s\n", string);
+
+	if (size >= 10)
+		limit = 10;
+	else
+		limit = size + 1;
+
+	printf("  first %ld bytes:", limit);
+
+	for (i = 0; i < limit; i++)
+		if (string[i] >= 0)
+			printf(" %02x", string[i]);
+		else
+			printf(" %02x", 256 + string[i]);
+
+	printf("\n");
+}
+
+/**
+ * print_python_list - Prints list information
+ *
+ * @p: Python Object
+ * Return: no return
+ */
+void print_python_list(PyObject *p)
+{
+	long int size, i;
+	PyListObject *list;
+	PyObject *obj;
+
+	size = ((PyVarObject *)(p))->ob_size;
+	list = (PyListObject *)p;
+
+	printf("[*] Python list info\n");
+	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Allocated = %ld\n", list->allocated);
+
+	for (i = 0; i < size; i++)
+	{
+		obj = ((PyListObject *)p)->ob_item[i];
+		printf("Element %ld: %s\n", i, ((obj)->ob_type)->tp_name);
+		if (PyBytes_Check(obj))
+			print_python_bytes(obj);
 	}
 }
