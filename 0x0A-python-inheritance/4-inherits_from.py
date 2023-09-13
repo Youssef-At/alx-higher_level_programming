148479410ea83acee6641ed11bc1b3ea34ee4d18
Youssef-At <72509411+Youssef-At@users.noreply.github.com>
2023-09-13 03:49:17 +0100

    Update 4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
-''' module: 4-inherits_from
-'''
+"""
+    This module returns the list of available attributes
+    and methods of an object
+"""
 
 
 def inherits_from(obj, a_class):
-    '''the object is an instance of a class that inherited (directly or indirectly)
-    obj: an object
-    a_class: a class
-    returns None
-    '''
-    return type(obj) != a_class and isinstance(obj, a_class)
+    """ Function that returns True/False if obj is an instance of a_class
+
+    Args:
+        obj: object
+        a_class: class type
+
+    Returns:
+        True if obj is an instance of a_class
+        False, otherwise
+    """
+    if type(obj) is a_class:
+        return False
+    return isinstance(obj, a_class)
