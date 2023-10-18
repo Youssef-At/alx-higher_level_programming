26617b02e901435eacd497f72c673374eb1a606e
Youssef-At <72509411+Youssef-At@users.noreply.github.com>
2023-10-18 03:08:23 +0100

    Update 1-create_database_if_missing.sql

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,3 @@
--- script that remove database hbtn_0c_0 does not fail
-DROP DATABASE IF EXISTS hbtn_0c_0;
+-- script that creates database hbtn_0c_0
+-- does not fail
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
